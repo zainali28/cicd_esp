0b62247a0db26757894d51745e95429fec354ad7
zainali28 <zainali123113@gmail.com>
2026-08-25 15:34:35 +0500

    formatted the data

--- cicd_esp.ino
+++ cicd_esp.ino
@@ -2,21 +2,18 @@
 
 int a = 0;
 
-void setup()
-{
-	Serial.begin(115200);
+void setup() {
+    Serial.begin(115200);
 }
 
-void loop()
-{
-	Serial.println("Hello World");
-	
-	switch(a)
-	{
-		case 1:
-			Serial.println("1");
-			break;
-	}
-	
-	delay(100);
+void loop() {
+    Serial.println("Hello World");
+
+    switch (a) {
+        case 1:
+            Serial.println("1");
+            break;
+    }
+
+    delay(100);
 }
